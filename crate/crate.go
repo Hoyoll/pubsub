c5906cbb876c9691f2555c956e28f751a32adb7b
Effendi <effendi.saputra01@gmail.com>
2025-03-26 21:35:23 +0700

    thread safe support i suppose

--- crate/crate.go
+++ crate/crate.go
@@ -1,34 +1,39 @@
 package crate
 
+import "sync"
+
 type Crate[K comparable, P any] struct {
-	Packet   map[K]P
+	Packet   *sync.Map
 	Fallback P
 }
 
 func New[K comparable, P any]() *Crate[K, P] {
-	crate := Crate[K, P]{
-		Packet: make(map[K]P),
+	crate := &Crate[K, P]{
+		Packet: &sync.Map{},
 	}
-	return &crate
+	return crate
 }
 
 func (this *Crate[K, P]) Get(name K) P {
-	packet, ok := this.Packet[name]
+	packet, ok := this.Packet.Load(name)
 	if !ok {
 		return this.Fallback
 	}
-	return packet
+	return packet.(P)
 }
 
 func (this *Crate[K, P]) Add(name K, packet P) {
-	this.Packet[name] = packet
+	this.Packet.Store(name, packet)
 }
 
 func (this *Crate[K, P]) Rmv(name K) {
-	_, ok := this.Packet[name]
-	if ok {
-		delete(this.Packet, name)
-	}
+	this.Packet.Delete(name)
+}
+
+func (this *Crate[K, P]) Iter(process func(K, P) bool) {
+	this.Packet.Range(func(key, value any) bool {
+		return process(key.(K), value.(P))
+	})
 }
 
 func (this *Crate[K, P]) Fall(packet P) {
